26c2dd4c76376d8cb859c4428c0c6f9a5b0b8b6a
GFrancV <gfrancv@hotmail.com>
2024-09-12 17:37:44 +0100

    refactor: Make basic UI to the site

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_INVITE_BOT_URL: string;
+  readonly PUBLIC_DISCORD_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
